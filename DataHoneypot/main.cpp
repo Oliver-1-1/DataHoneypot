5b2ea8ce8e4c8fe9ba62e07cb87da6531648f8bf
Oliver-Dahlen <oliverdahlen@gmail.com>
2023-11-03 00:07:19 +0100

    .

--- DataHoneypot/main.cpp
+++ DataHoneypot/main.cpp
@@ -5,7 +5,7 @@
 #include <TlHelp32.h>
 #include <Psapi.h>
 #include <sstream>
-
+#include <libloaderapi.h>
 #pragma comment(lib,"ntdll.lib")
 
 
@@ -42,19 +42,42 @@ void routine() {
 
 	//Empty all loaded pages in memory. Some pages will directly be loaded into memory again like .text section but the important 
 	//part is that we dont access any variable inside .data section. Like thread_id.
-	K32EmptyWorkingSet(handle);
+	K32EmptyWorkingSet(GetCurrentProcess());
 
-	for (int i = 0; i < 10000; i++) {
+	for (int i = 0; i < 100000000000000; i++) {//xd
 
-		K32GetProcessMemoryInfo(handle, &mem, sizeof(PROCESS_MEMORY_COUNTERS));
+		//store the base address the loaded Module
+		char* dllImageBase = (char*)GetModuleHandleA(NULL); //suppose hModule is the handle to the loaded Module (.exe or .dll)
 
-		//The page faults will instantly settle and be kept at the same count. If no other thread in the process(which is impossible
-		//Since we suspended them all. If a process tries to read from the data section with ReadProcessMemory it will be noticed :D.
-		//Attaching via cheat engine explodes the page fault count
-		std::cout << mem.PageFaultCount << std::endl;
-		Sleep(500);
+		//get the address of NT Header
+		auto dos = (PIMAGE_DOS_HEADER)dllImageBase;
+		if (dos->e_magic != IMAGE_DOS_SIGNATURE) return;
+		auto nt = (PIMAGE_NT_HEADERS)((BYTE*)dllImageBase + dos->e_lfanew);
+		if (nt->Signature != IMAGE_NT_SIGNATURE) return;
+
+		auto section_header = (PIMAGE_SECTION_HEADER)((BYTE*)(&nt->FileHeader) + sizeof(IMAGE_FILE_HEADER) + nt->FileHeader.SizeOfOptionalHeader);
+
+		for (auto j = 0; j < nt->FileHeader.NumberOfSections; j++, section_header++) {
+			if ((section_header->Characteristics & IMAGE_SCN_MEM_DISCARDABLE)) continue;
+			//if (execute_only) {
+			//	if (!(section_header->Characteristics & IMAGE_SCN_CNT_CODE)) continue;
+			//}
+			if (!strcmp((const char*)section_header->Name, ".data")) {
+				PSAPI_WORKING_SET_EX_INFORMATION working = { 0 };
+				working.VirtualAddress = (PVOID)((ULONGLONG)dllImageBase + section_header->VirtualAddress);
+				K32QueryWorkingSetEx(GetCurrentProcess(), &working, sizeof(working));
+				if (working.VirtualAttributes.Valid) {
+					std::cout << "Someone acceseed the page that was not the current process :D" << std::endl;
+				}
+				//std::cout << working.VirtualAttributes.Valid << std::endl;
+			}
+
+		}
+
+
 	}
-	
+	DoSuspendThread(GetCurrentProcessId(), thread_id, 0);
+
 
 }
 
@@ -71,5 +94,4 @@ void main() {
 	thread_id = std::stoi(ss.str());
 	s.join();
 
-	while(1){}
 }
